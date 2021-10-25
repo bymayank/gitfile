3a376c3c271d60e39311f6caec962e527d97f8e5
bymayank <67113424+bymayank@users.noreply.github.com>
2021-10-25 17:49:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import unittest
+
 from sum1 import sum
-class test1(unittest.testcase):
-  def test_sum(self):
-    data = [4,5,6]
-    res = sum(data)
-    self.assertequal(res,15)
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
